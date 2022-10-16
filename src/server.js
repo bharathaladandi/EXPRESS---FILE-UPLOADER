00918fe9b768d6ef738c27350452e884c1584a70
bharathaladandi <bharathaladandi@gmail.com>
2022-10-16 13:28:23 +0530

    Multer Added

--- src/server.js
+++ src/server.js
@@ -1,7 +1,20 @@
 const express  = require('express');
-
+const multer = require('multer');
 
 const app = express()
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./images")
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.originalname)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 
 
 
